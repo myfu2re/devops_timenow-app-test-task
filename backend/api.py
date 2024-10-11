b1fe19b996385d9a8beec5a6863de6517b6fe4e5
Pavlo Smolianinov <pavlo.smolianinov@medtronic.com>
2024-10-11 18:02:39 +0300

    for review v1

--- backend/api.py
+++ backend/api.py
@@ -3,16 +3,24 @@ from datetime import datetime
 
 app = Flask(__name__)  
 
-@app.route('/time')
+@app.route('/api')
 def time():
     now = datetime.now()
     responseBody = now.strftime("%H:%M:%S")
     return responseBody
 
-# modify response headers for all requests
+# uncomment to modify response headers for all requests
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 app.run(debug=True, port=8080, host="0.0.0.0")
+
+#if __name__ == "__main__":
+
+#    app.run(ssl_context=("nginx-selfsigned.crt", "nginx-selfsigned.key"), debug=True, port=8080, host="0.0.0.0")
+
+#app.run(ssl_context=('nginx-selfsigned.crt', 'nginx-selfsigned.key'))
+
+#app.run(ssl_context=('nginx-selfsigned.crt', 'nginx-selfsigned.key'), debug=True, port=8080, host="0.0.0.0")
